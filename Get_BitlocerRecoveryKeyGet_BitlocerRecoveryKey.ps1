d6dfce2bb6e26e05bac676a3d6b99d4c95da234f
martin.hartung <martin.hartung2019@outlook.com>
2023-04-12 15:27:59 +0200

    inital commit

---
+++ Get_BitlocerRecoveryKeyGet_BitlocerRecoveryKey.ps1
@@ -0,0 +1,18 @@
+﻿#
+# Enter a PC name and get the Bit-Locker recovery key
+#
+
+#enter pc between ""
+$computerName = ""
+
+#Get AD Computer Objects 
+$computer = Get-ADComputer -Identity $ComputerName
+
+#Get recoveryinformation
+$recoveryInformation = Get-ADObject -Filter 'objectClass -eq "msFVE-RecoveryInformation"' -SearchBase $computer.DistinguishedName -Properties *
+
+#write every BIT-Locker RecoveryKey that is found to the item in the terminal # its not always the newst
+foreach ($item in $recoveryInformation) {
+    $item.createTimeStamp
+    $item.'msFVE-RecoveryPassword'
+}
